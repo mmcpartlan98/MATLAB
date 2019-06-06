b1c883e9957c4ae93ffe44feef7fc55e1dfb5ea3
Matt McPartlan <mmcpartlan98@gmail.com>
2019-06-06 16:32:17 -0700

    June 6th work
    Fixed scoring algorithm, implemented a better searchX function

--- particleFinder.m
+++ particleFinder.m
@@ -13,7 +13,7 @@ readPath = [filePath  fileName  '.dpt'];
 % Set significance cutoff for background noise
 lowerSignificanceCutoff = 0;
 
-[xScale, correctedSpectra, rawSpectra, inflectionsFull] = dptRead(readPath, 60, 60);
+[xScale, correctedSpectra, rawSpectra, inflectionsFull] = dptRead(readPath, 25, 25);
 
 scArry = score(inflectionsFull, correctedSpectra, xScale);
 
@@ -45,9 +45,10 @@ figure;
 hold on;
 
 for particle = 1:numberOfParticles
-    particleSpectra(:, particle + 1) = generateParticleSpectrum(clusters, particle, rawSpectra, xScale);
+%    particleSpectra(:, particle + 1) = generateParticleSpectrum(clusters, particle, rawSpectra, xScale);
+    particleSpectra(:, particle + 1) = generateParticleSpectrum(clusters, particle, correctedSpectra, xScale);
     plot(xScale, particleSpectra(:, particle + 1));
-    set(gca, 'XDir','reverse');
+    %set(gca, 'XDir','reverse');
 end
 
 writePath = [filePath 'PendingSearches/' fileName 'Clustered' '.csv'];
@@ -60,7 +61,11 @@ b = bar3(scArry);
 for k = 1:length(b)
     zdata = b(k).ZData;
     b(k).CData = zdata;
-    b(k).FaceColor = 'interp';
+    if (b(k) == 0)
+        b(k).FaceColor = 'none';
+    else
+        b(k).FaceColor = 'interp';
+    end
 end
 
 figure;
@@ -69,5 +74,9 @@ c = bar3(clusters);
 for k = 1:length(c)
     zdata = c(k).ZData;
     c(k).CData = zdata;
-    c(k).FaceColor = 'interp';
+    if (c(k) == 0)
+        c(k).FaceColor = 'none';
+    else
+        c(k).FaceColor = 'interp';
+    end
 end
--- searchX.m
+++ searchX.m
@@ -1,18 +1,18 @@
-function [retRow, retCol] = searchX(x, y)
+function [retCol, retRow] = searchX(x, y)
 % Search X for Y. Returns [row, column] of either closest match or first encountered exact
 % match. X can be a vector or array, Y must be a value.
+x = x';
 [rows, columns] = size(x);
 maxVal = Inf;
 for iRow = 1:rows
     for iCol = 1:columns
-        x(iCol) = x(iCol) - y;
-        if (x(iCol) == 0)
+        if (x(iCol) - y == 0)
             retRow = iRow;
             retCol = iCol;
             return;
         end
-        if (x(iCol) < maxVal)
-            maxVal = x(iCol);
+        if (abs(x(iCol) - y) < maxVal)
+            maxVal = abs(x(iCol) - y);
             retRow = iRow;
             retCol = iCol;
         end
