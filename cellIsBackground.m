66007102b537e1aec5cd77cbecd25d07e70443e0
Matt McPartlan <mmcpartlan98@gmail.com>
2019-06-12 12:10:30 -0700

    June 11th Work
    Added software to manage background library

--- cellIsBackground.m
+++ cellIsBackground.m
@@ -1,66 +1,21 @@
-function [isBackground] = cellIsBackground(spectrum, number, backgroundPath)
-debug = 'n';
-bgs = 1;
+function [isBackground] = cellIsBackground(spectrum, correctedBG)
 spectrum = squeeze(smoothdata(spectrum, 'gaussian', 7));
-
-% Assume only one background to be loaded for now
-[BGXScale, ~, correctedBG, BGinflections] = dptRead(backgroundPath, 1, bgs, 'y');
-BGinflections = squeeze(BGinflections);
-
-
+[bgs, ~] = size(correctedBG);
+smallestError = Inf;
 for index = 1:bgs
-    spectrumInflections = diff(spectrum)./diff(BGXScale);
-    % spectrumInflections = diff(spectrumInflections)./diff(BGXScale(1:length(BGXScale) - 1));
-    
-    if (sum(abs(spectrumInflections)) ~= 0)
-        % Find zeros, flip to restore conventional spectral notation
-        zeroCrossingsXLocal = data_zeros(flip(BGXScale), flip(spectrumInflections))';
-        % Pad with zeros to concatenate
-        zeroCrossingsXLocal = [flip(sort(zeroCrossingsXLocal)') zeros(1, length(BGXScale) - length(zeroCrossingsXLocal))]';
-    else
-        zeroCrossingsXLocal = zeros(1, length(xScale));
-    end
-    
-    % DEBUG MODE INFLECTION POINT GRAPHING
-    if (debug == 'y')
-        zeroCrossingsMagnitude = zeros(length(BGXScale), 1);
-        for magIndex = 1:BGXScale
-            zeroCrossingsMagnitude(magIndex) = spectrum(searchX(BGXScale, zeroCrossingsXLocal(magIndex)));
-        end
-        plot(BGXScale, spectrum);
-        hold on;
-        stem(zeroCrossingsXLocal, zeroCrossingsMagnitude);
-        plot(BGXScale(1:length(BGXScale) - 1), spectrumInflections);
-        hold on;
-        
-        figure;
-        correctedBG = squeeze(correctedBG);
-        plot(BGXScale, correctedBG);
-        hold on;
-        for magIndex = 1:BGXScale
-            zeroCrossingsMagnitude(magIndex) = correctedBG(searchX(BGXScale, BGinflections(magIndex)));
-        end
-        stem(BGinflections, zeroCrossingsMagnitude);
-    end
-    
-    % Compare inflection points for each
-    errorSum = 0;
-    for derIndex = 1:length(zeroCrossingsXLocal)
-        errorSum = errorSum + abs(zeroCrossingsXLocal(derIndex) - BGinflections(derIndex));
-    end
-    
     % Attempt simple point-by-point subtraction-summation
     
-    errorSum = 0;
-    for derIndex = 1:length(zeroCrossingsXLocal)
-        errorSum = errorSum + abs(zeroCrossingsXLocal(derIndex) - BGinflections(derIndex));
-    end
+    error = abs(spectrum - correctedBG(index, :)');
+    errorSum = sum(error);
     
-    if (errorSum > 100000)
-        isBackground = true;
-    else
-        isBackground = false;
+    if (errorSum < smallestError)
+        smallestError = errorSum;
     end
-    fprintf('Stripping background %d (Score: %d)\n', number, errorSum);
+end
+
+if (smallestError < 50)
+    isBackground = true;
+else
+    isBackground = false;
 end
 end
--- data_zeros.m
+++ data_zeros.m
@@ -1,11 +1,9 @@
 function ZC = data_zeros(x,y)
 zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);                    % Returns Approximate Zero-Crossing Indices Of Argument Vector
 zxidx = zci(y);
-ZC = zeros(1, length(x));
 for k1 = 1:numel(zxidx)
     idxrng = max([1 zxidx(k1)-1]):min([zxidx(k1)+1 numel(y)]);
     xrng = x(idxrng);
-    yrng = y(idxrng);
-    ZC(k1) = interp1( yrng(:), xrng(:), 0, 'spline', 'extrap' );
+    ZC(k1) = xrng(1);
 end
 end
